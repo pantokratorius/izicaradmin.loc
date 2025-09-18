160e069edc709e34c966d97d1f997f1a2048d1c2
Erik <eitu@mail.ru>
2025-09-18 17:10:55 +0300

    .

--- database/migrations/2025_08_31_140559_create_clients_table.php
+++ database/migrations/2025_08_31_140559_create_clients_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
 
-            $table->string('first_name');   // имя
+            $table->string('first_name')->nullable();   // имя
             $table->string('last_name')->nullable();    // фамилия
             $table->string('middle_name')->nullable(); // отчество
             $table->string('phone')->unique()->nullable(); // телефон
