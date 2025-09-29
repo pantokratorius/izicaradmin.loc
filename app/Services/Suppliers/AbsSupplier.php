8bddd25e380625659836ae1988b1268cf36078cd
Erik <eitu@mail.ru>
2025-09-29 16:31:44 +0300

    .

--- app/Services/Suppliers/AbsSupplier.php
+++ app/Services/Suppliers/AbsSupplier.php
@@ -49,7 +49,7 @@ class AbsSupplier implements SupplierInterface
                 return [];
             }
 
-            return collect($json ?? [])->map(function ($item) {
+            return collect($json ?? [])->map(function ($item) { dd($item);
                 return [
                      'name'        => $item['product_name'],
                     'part_make'   => $item['brand'],
