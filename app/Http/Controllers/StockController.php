2dd1d8b54e9aa72259ad37701b13dc27c4fd7404
Erik <eitu@mail.ru>
2025-10-14 15:48:21 +0300

    .

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class StockController extends Controller
 {
     public function index()
-    {  dd(function_exists('imap_open'));
+    {
         $stocks = Stock::orderBy('id', 'desc')->paginate(20); 
         return view('stocks.index', compact('stocks')); 
     }
