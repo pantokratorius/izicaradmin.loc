c2bb501e4662e5ed65e63b6859f96640cc5a81db
Erik <eitu@mail.ru>
2025-11-19 09:36:42 +0300

    .

--- app/Services/Suppliers/Autosputnik.php
+++ app/Services/Suppliers/Autosputnik.php
@@ -47,63 +47,52 @@ class Autosputnik implements SupplierInterface
     }
 
 
-
-public function asyncSearchItems(Client $client, string $article, ?string $brand = null): PromiseInterface
+    public function asyncSearchItems(Client $client, string $article, ?string $brand = null): PromiseInterface
 {
-
-   Log::info('Sending request to AutoSputnik API', [
-        'query' => [
-            'articul' => $article,
-            'brand'   => $brand,
-            'analogi' => true,
-            'tranzit' => false,
-        ]
-    ]);
-
-
-    return $client->postAsync('https://newapi.auto-sputnik.ru/products/getproducts', [
-        'headers' => [
-            'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaXppY2FyMiIsInVzZXJpZCI6Ijg4MjEyIiwiZXhwIjoxNzYzNDY0NDk0LCJpc3MiOiJhcGlhdXRvc3B1dG5payIsImF1ZCI6ImFwaWF1dG9zcHV0bmlrY2xpZW50In0.uaWqS5sORBDrv9OzRvQbFvRgaogunBWwmsZavmn5oKE',
+    $formParams = [
+        'options' => [
+            'login'   => 'izicar2',
+            'pass'    => '123456',
+            'datatyp' => 'JSON',
+            'storage' => 'as',
         ],
-        [
+        'data' => [
             'articul' => $article,
-            'brand'   => $brand,
-            'analogi' => true,
-            'tranzit' => false,
         ],
-    ])->then(function ($response) {  
+    ];
 
-        // ---- READ RESPONSE ----
+    if (!empty($brand)) {
+        $formParams['data']['brand'] = $brand;
+    }
+
+    return $client->postAsync('https://api.auto-sputnik.ru/search_result.php', [
+        'form_params' => $formParams,
+    ])->then(function ($response) use ($formParams) {
         $body = $response->getBody()->getContents();
-        $body = mb_convert_encoding($body, 'UTF-8', 'UTF-8');
-
-  Log::info("AutoSputnik API response for article ", [
-            'body' => $body
-        ]);
+        $body = mb_convert_encoding($body, 'UTF-8', 'Windows-1251');
 
         $json = json_decode($body, true);
+        // Log::info("aaaaaaaaaaaaaaaa:\n" .  json_encode($formParams, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)  . "\n" . json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . "\n\n" 
+            
+        // );
 
-        if (!isset($json['data']) || !is_array($json['data'])) {
+        if (empty($json['requestAnswer']) || !is_array($json['requestAnswer'])) {
             return [];
         }
 
-        // ---- TRANSFORM DATA INTO YOUR FORMAT ----
-        return collect($json['data'])->map(function ($item) {
-
+        return collect($json['requestAnswer'])->map(function ($item) {
             return [
-                'name'          => $item['name'] ?? null,
-                'part_make'     => $item['brand']['name'] ?? null,
-                'part_number'   => $item['articul'] ?? null,
-                'quantity'      => $item['quantity'] ?? null,
-                'price'         => $item['price'] ?? null,
-                'delivery'      => $item['delivery_day'] ?? null,
-                'warehouse'     => null,
+                'name'        => $item['NAME_TOVAR'] ?? null,
+                'part_make'   => $item['BRA_BRAND'] ?? null,
+                'part_number' => $item['ARTICUL'] ?? null,
+                'quantity'    => round($item['STOCK'], 0) ?? null,
+                'price'       => $item['NEW_COST'] ?? null,
+                'delivery'    => $item['DAYOFF'] ?? null,
+                'warehouse'   => $item['PRICE_NAME'] ?? null,
             ];
-
         })->toArray();
     });
 }
-
 }
 
 
